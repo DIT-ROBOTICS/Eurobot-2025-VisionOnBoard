305d647b47ce6a63a8a0400fd855931192f1e54b
FJDXXX <fujiade0518@gmail.com>
2025-05-28 01:33:49 +0800

    Add parameter "check_angular" and backup

---
+++ docker/scripts/temp/avoidance-pkg/include/voxelgrid-cube.h
@@ -0,0 +1,23 @@
+#ifndef _CUBE_H_
+#define _CUBE_H_
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+
+class Cube {
+public:
+    double x_low = -0.5, x_high = 0.5, y_low = -0.5, y_high = 0.5, z_low = 0, z_high = 1;
+    double leaf_size = 0.05;
+    double min_z = 100;
+
+    void filterPointCloud(const sensor_msgs::msg::PointCloud2& input, sensor_msgs::msg::PointCloud2& output);
+    bool stopRobot(const sensor_msgs::msg::PointCloud2::SharedPtr& cloud_msg);
+};
+
+#endif // _CUBE_H_
---
+++ docker/scripts/temp/avoidance-pkg/src/voxelgrid-cube.cpp
@@ -0,0 +1,54 @@
+#include "voxelgrid-cube.h"
+
+extern double x_low, x_high, y_low, y_high, z_low, z_high, leaf_size, min_z;
+
+void Cube::filterPointCloud(const sensor_msgs::msg::PointCloud2& cloud_msg, sensor_msgs::msg::PointCloud2& output) {
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl::fromROSMsg(cloud_msg, *cloud);
+
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+    // Filter X
+    pass.setInputCloud(cloud);
+    pass.setFilterFieldName("x");
+    pass.setFilterLimits(x_low, x_high);
+    pass.filter(*cloud_filtered);
+
+    // Filter Y
+    pass.setInputCloud(cloud_filtered);
+    pass.setFilterFieldName("y");
+    pass.setFilterLimits(y_low, y_high);
+    pass.filter(*cloud_filtered);
+
+    // Filter Z
+    pass.setInputCloud(cloud_filtered);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(z_low, z_high);
+    pass.filter(*cloud_filtered);
+
+    // Voxel Grid
+    pcl::VoxelGrid<pcl::PointXYZRGB> vg;
+    vg.setInputCloud(cloud_filtered);
+    vg.setLeafSize(leaf_size, leaf_size, leaf_size);
+    vg.filter(*cloud_filtered);
+
+    pcl::toROSMsg(*cloud_filtered, output);
+}
+
+bool Cube::stopRobot(const sensor_msgs::msg::PointCloud2::SharedPtr& cloud_msg) {
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl::fromROSMsg(*cloud_msg, *cloud);
+
+    int counter = 0;
+    for(const auto& point : cloud->points) {
+        if(point.z) {
+            counter++;
+            if(counter > 50) {
+                counter = 0;
+                return true;
+            }
+        }
+    }
+    return false;
+}
